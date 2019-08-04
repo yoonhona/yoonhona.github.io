153c8f3b0312ad5d9484998cfdaaa3dd7e9aae87
yoonhona <lucky72nyh@gmail.com>
2019-08-04 16:10:53 +0900

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: '🐾밥이',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Test', link: '/test/' },
+            { text: 'github', link: 'https://github.com/yoonhona' },
+        ]
+    }
+}
